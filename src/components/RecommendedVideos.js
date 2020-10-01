c7a19135296244854db11b1514fb75aeea806e81
dasu5 <dasunianupama@gmail.com>
2020-10-01 12:35:04 +0530

    header, sidebar and main content added

---
+++ src/components/RecommendedVideos.js
@@ -0,0 +1,72 @@
+import React from "react";
+import "../styles/RecommendedVideos.css";
+import VideoCard from "./VideoCard";
+
+function RecommendedVideos() {
+  return (
+    <div className="recommendedVideos">
+      <h2>Recommended</h2>
+
+      <div className="recommendedVideos__videos">
+        <VideoCard
+          title="Become a React Js Developer in 2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="logo192.png"
+          channel="CleverProgrammar"
+          image="logo192.png"
+        />
+        <VideoCard
+          title="Become a React Js Developer in 2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="logo192.png"
+          channel="CleverProgrammar"
+          image="logo192.png"
+        />
+        <VideoCard
+          title="Become a React Js Developer in 2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="logo192.png"
+          channel="CleverProgrammar"
+          image="logo192.png"
+        />
+        <VideoCard
+          title="Become a React Js Developer in 2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="logo192.png"
+          channel="CleverProgrammar"
+          image="logo192.png"
+        />
+        <VideoCard
+          title="Become a React Js Developer in 2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="logo192.png"
+          channel="CleverProgrammar"
+          image="logo192.png"
+        />
+        <VideoCard
+          title="Become a React Js Developer in 2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="logo192.png"
+          channel="CleverProgrammar"
+          image="logo192.png"
+        />
+        <VideoCard
+          title="Become a React Js Developer in 2020"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="logo192.png"
+          channel="CleverProgrammar"
+          image="logo192.png"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default RecommendedVideos;
---
+++ src/components/SearchPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/SearchPage.css";
+
+function SearchPage() {
+  return <div className="searchPage"></div>;
+}
+
+export default SearchPage;
